d9e46636560854db8d343a93511f941aeca50b2f
Sodre <mateus.sodre.s@gmail.com>
2023-03-02 12:56:44 -0300

    commit dos exercicios java collections aula 3

---
+++ JavaCollectionsAlura/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+import java.util.List;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as coleçoes do Java", "Paulo Silveira");
+		
+		//javaColecoes.getAulas().add(new Aula("Trabalando com ArrayLists" , 21));//
+		javaColecoes.adiciona(new Aula("Trabalando com ArrayLists" , 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula" , 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções" , 22));
+		
+		System.out.println(javaColecoes.getAulas());
+
+	}
+}
